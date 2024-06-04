45933514a75b2450f528f91dcbc7faa472b7111c
luiscacostas <acosta.luisc15@gmail.com>
2024-06-04 09:17:30 +0200

    creacion de graficas

---
+++ script.js
@@ -0,0 +1,91 @@
+
+const firstCallApi = async()=>{
+    try {
+        const respuesta = await fetch('https://swapi.dev/api/films/')
+        const data = await respuesta.json()
+        const peliculas = data.results
+        const tituloPeliculas = peliculas.map(pelicula => pelicula.title)
+        const yearData = peliculas.map(pelicula=>pelicula.release_date)
+        const año = yearData.map( d => new Date( d ).getFullYear() );
+        return paintLines(año, tituloPeliculas)
+    } catch (error) {
+    }
+}
+firstCallApi()
+
+const paintLines = (año, tituloPeliculas)=>{
+    var defaultOptions = {
+        axisY: {
+          onlyInteger: true
+        },
+    }
+    new Chartist.Line('.ct-chart', {
+        labels: tituloPeliculas,
+        series: [año]
+      }, defaultOptions);
+}
+
+const secondCallApi = async()=>{
+try {
+   const respuesta = await fetch('https://swapi.dev/api/people/') 
+   const data = await respuesta.json()
+   const personajes = data.results
+   const filmsLength = personajes.map(personaje=>personaje.films.length)
+   console.log(filmsLength)
+   const nombrePersonaje = personajes.map(personaje => personaje.name)
+   console.log(nombrePersonaje)
+   console.log({personajes})
+   return paintBarras(nombrePersonaje, filmsLength)
+} catch (error) {
+    console.log(error)
+}
+}
+secondCallApi()
+
+const paintBarras = (nombrePersonaje, filmsLength)=>{
+    new Chartist.Bar('.ct-chart2', {
+        labels: nombrePersonaje,
+        series: [filmsLength]
+      }, {
+        stackBars: true,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value.split(/\s+/).map(function(word) {
+              return word[0];
+            }).join('');
+          },
+          onlyInteger:true
+        },
+        axisY: {
+          offset: 20,
+          onlyInteger: true
+        }
+      }, [
+        // Options override for media > 400px
+        ['screen and (min-width: 400px)', {
+          reverseData: true,
+          horizontalBars: true,
+          axisX: {
+            labelInterpolationFnc: Chartist.noop,
+          },
+          axisY: {
+            offset: 60,
+          }
+        }],
+        // Options override for media > 800px
+        ['screen and (min-width: 800px)', {
+          stackBars: false,
+          seriesBarDistance: 10
+        }],
+        // Options override for media > 1000px
+        ['screen and (min-width: 1000px)', {
+          reverseData: false,
+          horizontalBars: false,
+          seriesBarDistance: 15
+        }]
+      ]);
+      
+}
+
+
+
